f7ad670c38047d5ce308a940f9d34f9e033ed699
Johan Christoffersson <chstoffesson@gmail.com>
2021-06-07 07:27:07 +0000

    Added a UniqueConstraint for the user_membership table.

--- tagbot/database.py
+++ tagbot/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (Column, String, Integer, Text, ForeignKey, Table, MetaData, select, and_, insert)
+from sqlalchemy import (Column, String, Integer, Text, ForeignKey, UniqueConstraint, Table, MetaData, select, and_, insert)
 from sqlalchemy.engine.base import Engine
 from mautrix.types import UserID, EventID, RoomID
 
@@ -21,7 +21,8 @@ class TagDatabase:
         self.user_memberships = Table("tagbot_user_memberships", meta,
                                       Column("um_id", Integer, primary_key=True, autoincrement=True),
                                       Column("tag_group", Integer, ForeignKey("tagbot_tag_groups.tg_id", ondelete="CASCADE")),
-                                      Column("user_id", String(255), primary_key=True))
+                                      Column("user_id", String(255)),
+                                      UniqueConstraint("tag_group", "user_id",name="tag_group_user_id_unique_idx"))
         meta.create_all()
 
     def _check_if_tag_exists(self, tag: str) -> bool:

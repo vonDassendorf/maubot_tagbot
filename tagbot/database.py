9337100ec5247961250881d48f8d681e77629b02
vonDassendorf <chstoffesson@gmail.com>
2021-06-17 01:28:15 +0200

    Creating new per-room tag groups and adding users to tag group support added.

--- tagbot/database.py
+++ tagbot/database.py
@@ -27,9 +27,10 @@ class TagDatabase:
                                       UniqueConstraint("tag_group", "user_id", name="tag_group_user_id_unique_idx"))
         meta.create_all()
 
-    def _check_if_tag_exists(self, tag: str) -> bool:
+    def _check_if_tag_exists(self, tag: str, room_id: str) -> bool:
         if self.db.execute(
-                select([self.tag_groups.c.group_tag]).where(self.tag_groups.c.group_tag == tag)).first() is not None:
+                select([self.tag_groups.c.group_tag]).where(and_(self.tag_groups.c.group_tag == tag,
+                                                                 self.tag_groups.c.room_id == room_id))).first() is not None:
             return True
         else:
             return False
@@ -50,9 +51,10 @@ class TagDatabase:
             tags.append(row[0])
         return tags
 
-    def get_members_of_group_by_tag(self, tag: str):
+    def get_members_of_group_by_tag(self, tag: str, room_id: str):
         join = self.user_memberships.join(self.tag_groups, self.user_memberships.c.tag_group == self.tag_groups.c.tg_id)
-        stmt = select([self.user_memberships.c.user_id]).select_from(join).where(self.tag_groups.c.group_tag == tag)
+        stmt = select([self.user_memberships.c.user_id]).select_from(join).where(
+            and_(self.tag_groups.c.group_tag == tag, self.tag_groups.c.room_id == room_id))
         rows = self.db.execute(stmt)
         user_ids = []
         for row in rows:
@@ -60,17 +62,19 @@ class TagDatabase:
         yield user_ids
 
     def insert_new_tag(self, tag: str, room_id: str) -> bool:
-        if not self._check_if_tag_exists(tag):
+        if not self._check_if_tag_exists(tag, room_id):
             self.db.execute(insert(self.tag_groups).values(group_tag=tag, room_id=room_id))
             return True
         else:
             return False
 
-    def insert_user_membership(self, tag: str, user_id: str):
-        tag_id = self.db.execute(select([self.tag_groups.c.tg_id]).where(self.tag_groups.c.group_tag == tag)).first()
-        self.bot.log.info(tag_id)
-        if not self._check_if_user_is_member(tag_id, user_id):
-            self.db.execute(insert(self.user_memberships).values(tag_group=tag_id, user_id=user_id))
-            return True
-        else:
-            return False
+    def insert_user_membership(self, tag: str, user_id: str, room_id: str):
+        res = self.db.execute(select([self.tag_groups.c.tg_id]).where(and_(self.tag_groups.c.group_tag == tag, self.tag_groups.c.room_id == room_id))).first()
+        if res is not None:
+            tag_id = res[0]
+            self.bot.log.info(tag_id)
+            if not self._check_if_user_is_member(tag_id, user_id):
+                self.db.execute(insert(self.user_memberships).values(tag_group=tag_id, user_id=user_id))
+                return True
+            else:
+                return False
--- tagbot/tagbot.py
+++ tagbot/tagbot.py
@@ -27,20 +27,19 @@ class TagBot(Plugin):
         content = TextMessageEventContent(msgtype=MessageType.TEXT, body=f"{message}")
         await self.client.send_message(evt.room_id, content)
 
-    @tag.subcommand(name="adduser", aliases="addusr", help="Add a user that will be tagged when using specified tag. !tag adduser <tagname> <userid>")
-    @command.argument("args", pass_raw=True, required=True)
-    async def add_user_to_tag(self, evt: MessageEvent, args: str):
-        args = args.split(' ')
-        tag = args.pop(0)
-        self.log.info(args)
-        for arg in args:
-            user_id = arg
-            if self.db.insert_user_membership(tag, user_id):
-                message = f"User {user_id} added to tag group {tag}."
-            else:
-                message = f"User {user_id} already belong to tag group {tag}."
-            content = TextMessageEventContent(msgtype=MessageType.TEXT, body=f"{message}")
-            await self.client.send_message(evt.room_id, content)
+    @tag.subcommand(name="adduser", aliases="addusr",
+                    help="Add a user that will be tagged when using specified tag. !tag adduser <tagname> <userid>")
+    @command.argument("tag", required=True)
+    @command.argument("user_id", required=True)
+    async def add_user_to_tag(self, evt: MessageEvent, tag: str, user_id: str):
+        if self.db.insert_user_membership(tag, user_id, evt.room_id):
+            message_html = f"User <a href='https://matrix.to/#/{user_id}'>{user_id}</a> added to tag group {tag} in <a href='https://matrix.to/#/{evt.room_id}'>{evt.room_id}</a>."
+            message = f"User [{user_id}](https://matrix.to/#/{user_id}) added to tag group {tag} in [{evt.room_id}](https://matrix.to/#/{evt.room_id})."
+        else:
+            message_html = f"User <a href='https://matrix.to/#/{user_id}'>{user_id}</a> already belong to tag group {tag} in <a href='https://matrix.to/#/{evt.room_id}'>{evt.room_id}</a>."
+            message = f"User [{user_id}](https://matrix.to/#/{user_id}) already belong to tag group {tag} in [{evt.room_id}](https://matrix.to/#/{evt.room_id})."
+        content = TextMessageEventContent(msgtype=MessageType.TEXT, body=f"{message}", format=Format.HTML, formatted_body=f"{message_html}")
+        await self.client.send_message(evt.room_id, content)
         return
 
     async def everyone(self, evt: MessageEvent, message: str = '') -> None:
@@ -52,20 +51,25 @@ class TagBot(Plugin):
             if member.sender != botUid:
                 users_html += f" <a href='https://matrix.to/#/{member.sender}'>{member.sender}</a> "
                 users += f" [{member.sender}](https://matrix.to/#/{member.sender}) "
-        content = TextMessageEventContent(msgtype=MessageType.TEXT, body=f"{users} {message}", format=Format.HTML, formatted_body=f"{users_html} {message}")
+        content = TextMessageEventContent(msgtype=MessageType.TEXT, body=f"{users} {message}", format=Format.HTML,
+                                          formatted_body=f"{users_html} {message}")
         await self.client.send_message(evt.room_id, content)
 
     @command.passive("^@")
-    @command.argument('message', pass_raw=True, required=False)
-    async def tag_everyone(self, evt: MessageEvent, match: Tuple[str], message: str = ''):
-        if match[0].startswith("@everyone"):
+    async def tag_everyone(self, evt: MessageEvent, match: Tuple[str]):
+        message = ''
+        rcv_msg = re.split(r"\s", match[0], 2, re.I)
+        tag = rcv_msg[0].lower()
+        if len(rcv_msg) > 1:
+            message = rcv_msg[1]
+        if tag == "@everyone":
             await self.everyone(evt, message=message)
             return
         else:
             registered_tags = self.db.get_all_tags()
             for reg_tag in registered_tags:
-                if match[0].startswith("@"+reg_tag):
-                    members = self.db.get_members_of_group_by_tag(reg_tag)
+                if tag == "@" + reg_tag.lower():
+                    members = self.db.get_members_of_group_by_tag(reg_tag, evt.room_id)
                     users = ''
                     users_html = ''
                     for member in members:
